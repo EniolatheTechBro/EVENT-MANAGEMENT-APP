bd7783266f23edb457bce8665308dfed9354d64f
EniolatheTechBro <ennyisrael@gmail.com>
2024-01-03 15:34:08 +0100

    modified some files

---
+++ DB/DB.js
@@ -0,0 +1,4 @@
+const mongoDBURL = 'mongodb+srv://ennyisrael:Test1234@cluster0.dq1xfx1.mongodb.net/FirstDatabase'
+// const mongoDBURL2 = 'mongodb+srv://bobby:12345@cluster0.dq1xfx1.mongodb.net/FirstDatabase'
+
+module.exports = mongoDBURL
--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const mongoDBURL = 'mongodb+srv://ennyisrael:Test1234@cluster0.dq1xfx1.mongodb.net/FirstDatabase'
-// const mongoDBURL2 = 'mongodb+srv://bobby:12345@cluster0.dq1xfx1.mongodb.net/FirstDatabase'
-
+const mongoDBURL = require('../DB/DB')
 async function connectDB(){
     try {
         await mongoose.connect(mongoDBURL)
